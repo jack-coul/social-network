07ad9ffbe669d93ccfdb61433937fa540d5aff73
Tamerlan <tamerlan.musaev@mail.ru>
2022-03-30 10:46:33 +0300

    first commit

---
+++ src/components/ribbon/Ribbon.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Ribbon.module.css'
+
+const Ribbon = () => {
+    return (
+        <div className={styles.ribbonWrapper}>
+            
+        </div>
+    );
+};
+
+export default Ribbon;
