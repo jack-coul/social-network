d87adafc5c427103b3ded6598e76785c86f79365
Tamerlan <tamerlan.musaev@mail.ru>
2022-03-31 11:30:27 +0300

    Messages Component finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./header/Header";
+import Messages from "./messages/Messages";
 
 function App() {
   return (
     <div>
       <Header />
       <div className="App">
-
+      <Routes>
+        <Route path="/messages" element={<Messages />} />
+      </Routes>
       </div>
     </div>
   );
--- src/components/header/messagePage/MessagePage.jsx
+++ src/components/header/messagePage/MessagePage.jsx
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styles from "./MessagePage.module.css";
 
 const MessagePage = () => {
   return (
     <div className={styles.messageWrapper}>
-        <div className={styles.numberOfMessages}>0</div>
-      <img
-        width={24}
-        height={24}
-        src="https://cdn-icons-png.flaticon.com/512/2983/2983788.png"
-        alt=""
-      />
+      <div className={styles.numberOfMessages}>0</div>
+      <Link to="/messages">
+        <img
+          width={24}
+          height={24}
+          src="https://cdn-icons-png.flaticon.com/512/2983/2983788.png"
+          alt=""
+        />
+      </Link>
     </div>
   );
 };
