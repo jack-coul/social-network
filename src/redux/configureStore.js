cb316b95569574bf941ae97c8f6c22905537ed39
Islam Komurzoeve <jackcoul0677l@gmailr.com>
2022-03-31 14:54:23 +0300

    writed configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,10 +3,22 @@ import { composeWithDevTools } from "@redux-devtools/extension";
 import thunk from "redux-thunk";
 import application from "./features/application";
 import posts from "./features/posts";
+import conversation from "./features/conversation";
+import group from "./features/group";
+import likes from "./features/likes";
+import message from "./features/message";
+import notification from "./features/notification";
+import saves from "./features/saves";
 
 const combineRouter = combineReducers({
   application,
   posts,
+  conversation,
+  group,
+  likes,
+  message,
+  notification,
+  saves,
 });
 
 const store = createStore(
