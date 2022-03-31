50f6632e1468562e90d9b176f01d45b100e0b0c0
Tamerlan <tamerlan.musaev@mail.ru>
2022-03-31 16:42:17 +0300

    fix

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './Profile.module.css'
 import Posts from './posts/Posts';
 import Saved from '../saved/Saved';
+import { Link } from 'react-router-dom';
+import HeaderForUser from './headerForUser/HeaderForUser';
 
 
 const Profile = () => {
@@ -9,57 +11,8 @@ const Profile = () => {
     
     return (
         <div className={styles.profile}>
-            <div className={styles.header}>
-                <div className={styles.headerImg}>
-                    <div className={styles.headerImgWrap}>
-                        <img src="https://st2.depositphotos.com/1009634/7235/v/950/depositphotos_72350117-stock-illustration-no-user-profile-picture-hand.jpg" alt='profile'/>
-                    </div>
-                </div>
-                <div className={styles.headerDescription}>
-                    <div className={styles.descriptionEdit}>
-                        <h2>adam.baker.786</h2>
-                        <div className={styles.descriptionEditButton}>
-                            <a className={styles.editButton} href="fjf">Редактировать профиль</a>
-                        </div>
-                        <div className={styles.descriptionEditSettings}>
-                            <svg aria-label="Параметры" class="_8-yf5 " color="#262626" fill="#262626" height="24" role="img" viewBox="0 0 24 24" width="24">
-                                <circle cx="12" cy="12" fill="none" r="8.635" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></circle>
-                                <path d="M14.232 3.656a1.269 1.269 0 01-.796-.66L12.93 2h-1.86l-.505.996a1.269 1.269 0 01-.796.66m-.001 16.688a1.269 1.269 0 01.796.66l.505.996h1.862l.505-.996a1.269 1.269 0 01.796-.66M3.656 9.768a1.269 1.269 0 01-.66.796L2 11.07v1.862l.996.505a1.269 1.269 0 01.66.796m16.688-.001a1.269 1.269 0 01.66-.796L22 12.93v-1.86l-.996-.505a1.269 1.269 0 01-.66-.796M7.678 4.522a1.269 1.269 0 01-1.03.096l-1.06-.348L4.27 5.587l.348 1.062a1.269 1.269 0 01-.096 1.03m11.8 11.799a1.269 1.269 0 011.03-.096l1.06.348 1.318-1.317-.348-1.062a1.269 1.269 0 01.096-1.03m-14.956.001a1.269 1.269 0 01.096 1.03l-.348 1.06 1.317 1.318 1.062-.348a1.269 1.269 0 011.03.096m11.799-11.8a1.269 1.269 0 01-.096-1.03l.348-1.06-1.317-1.318-1.062.348a1.269 1.269 0 01-1.03-.096" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path>    
-                            </svg>
-                        </div>
-                    </div>
-                    <div className={styles.descriptionPosts}>
-                        <div><b>36</b> публикаций</div>
-                        <div><b>184</b> подписчиков</div>
-                        <div><b>194</b> подписок</div>
-                    </div>
-                    <div className={styles.descriptionName}>
-                         Sad.Adam
-                    </div>
-                </div>
-            </div>
-            <div className={styles.menu}>
-                <div className={styles.menuMain}>
-                    <a className={styles.menuLink} href='kjkj'>
-                        <svg aria-label="" class="_8-yf5 " color="#262626" fill="#262626" height="12" role="img" viewBox="0 0 24 24" width="12"><rect fill="none" height="18" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" width="18" x="3" y="3"></rect><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="9.015" x2="9.015" y1="3" y2="21"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="14.985" x2="14.985" y1="3" y2="21"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="9.015" y2="9.015"></line><line fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" x1="21" x2="3" y1="14.985" y2="14.985"></line></svg>
-                        <div className={styles.menuText}>ПУБЛИКАЦИИ</div>
-                    </a>
-                </div>
-                <div className={styles.menuMain}>
-                    <a className={styles.menuLink} href='hjkl'>
-                        <svg aria-label="" class="_8-yf5 " color="#8e8e8e" fill="#8e8e8e" height="12" role="img" viewBox="0 0 24 24" width="12"><path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.6 0 12 0zm0 22.5C6.2 22.5 1.5 17.8 1.5 12S6.2 1.5 12 1.5 22.5 6.2 22.5 12 17.8 22.5 12 22.5zm5-11.8l-6.8-3.9c-.5-.3-1-.3-1.5 0-.4.3-.7.7-.7 1.3v7.8c0 .5.3 1 .8 1.3.2.1.5.2.8.2s.5-.1.8-.2l6.8-3.9c.5-.3.8-.8.8-1.3s-.5-1-1-1.3zm-7.5 5.2V8.1l6.8 3.9-6.8 3.9z"></path></svg>
-                        <div className={styles.menuText}>ВИДЕО</div>
-                    </a>
-                </div>
-                <div className={styles.menuMain}>
-                    <a className={styles.menuLink} href='sdfg'>
-                        <svg aria-label="" class="_8-yf5 " color="#8e8e8e" fill="#8e8e8e" height="12" role="img" viewBox="0 0 24 24" width="12"><polygon fill="none" points="20 21 12 13.44 4 21 4 3 20 3 20 21" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"></polygon></svg>
-                        <div className={styles.menuText}>СОХРАНЕННОЕ</div>
-                    </a>
-                </div>
-            </div>
+            <HeaderForUser />
             <Posts />
-            <Saved />
         </div>
     );
 };
