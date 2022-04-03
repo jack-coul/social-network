c23175a35a5db03c3c9b567c5372fb4497616fca
Ibragim <dragunov.lion@gmail.com>
2022-04-03 19:05:25 +0300

    Ибрагим: сделал невозможное возможным (теперь у нас есть messenger молниеносного реагирования)

--- src/components/messages/Messages.jsx
+++ src/components/messages/Messages.jsx
@@ -5,65 +5,82 @@ import { getMessage, postMessages } from "../../redux/features/message";
 import Conversation from "./conversation/Conversation";
 import Message from "./message/Message";
 import styles from "./Messages.module.css";
-import {io} from "socket.io-client"
+import { io } from "socket.io-client";
 import { useRef } from "react";
 
 const Messages = () => {
   const state = useSelector((state) => state.conversation.conversation);
-  const message = useSelector(state=> state.message.message)
+  const message = useSelector((state) => state.message.message);
   const dispatch = useDispatch();
   const [conversation, setConversation] = useState();
   const [currentChat, setCurrentChat] = useState(null);
   const [messages, setMessages] = useState([]);
-  const [newMessages, setNewMessages]= useState("")
-  const [arrivelMessages, setArrivelMessages]= useState(null)
+  const [newMessages, setNewMessages] = useState("");
+  const [arrivelMessages, setArrivelMessages] = useState(null);
+  const [tryMessages, setTryMessages] = useState(null)
 
-const socket = useRef()
-  const userId = useSelector((state)=>state.application.id)
+  const socket = useRef();
+  const userId = useSelector((state) => state.application.id);
 
+  useEffect(() => {
+    socket.current = io("http://localhost:8900");
+
+  });
   useEffect(()=>{
-    socket.current = io("http://localhost:8900")
-    socket.current.on("getMessage", data =>{
+  
+    socket.current.on("getMessage", (data) => {
+      console.log(data);
       setArrivelMessages({
         sender: data.senderId,
         text: data.text,
-        createdAt: Date.now()
-      })
-    })
-  })
+      });
+      console.log(arrivelMessages)
+    });
+    
+  },arrivelMessages)
 
-  useEffect(()=>{
-    arrivelMessages && currentChat?.members.includes(arrivelMessages.sender)&&
-    setMessages((prev)=>[...prev, arrivelMessages])
-  },[arrivelMessages, currentChat])
+  useEffect(() => {
+    arrivelMessages &&
+      currentChat?.members.includes(arrivelMessages.sender) &&
+      setTryMessages((prev)=>[...prev,arrivelMessages]);
 
-useEffect(()=>{
-  socket.current.emit("AddUser", userId )
-  socket.current.on("getUsers", (users) =>{
-    console.log(users)
-  })
-}, [dispatch])
-useEffect(()=>{
-  dispatch(getMessage(currentChat?._id))
-  setMessages(message)
-  console.log(messages)
-},[currentChat])
-  
+      // setMessages((prev) => [...prev, arrivelMessages]);
+      // console.log(tryMessages)
+  });
+
+
+  useEffect(() => {
+    socket.current.emit("addUser", userId);
+    // socket.current.on("getUsers", (users) => {
+    // });
+  }, [dispatch]);
+  useEffect(() => {
+    dispatch(getMessage(currentChat?._id));
+    setMessages(message);
+  }, [currentChat]);
+
   useEffect(() => {
     dispatch(getConversation());
   }, [dispatch]);
-  
-  const handleSubmit =  (conversationId,text)=>{
-    dispatch(postMessages(conversationId, text))
-    
-    const receiverId = currentChat?.members.find((member)=>member._id !==userId)
+  const handleSubmit = (conversationId, text) => {
+    dispatch(postMessages(conversationId, text));
+    const messagePost = {
+      sender: userId,
+      text: text,
+      conversationId: currentChat._id
+    }
+    const receiverId = currentChat?.members.find(
+      (member) => member._id !== userId
+      
+    );
     socket.current.emit("sendMessage", {
       senderId: userId,
-      receiverId,
-      text: newMessages
-    })
-  }
-  
+      receiverId: receiverId._id,
+      text: newMessages,
+    });
+    setMessages([...messages, messagePost])
+  };
+
   return (
     <>
       <div className={styles.messages}>
@@ -83,7 +100,11 @@ useEffect(()=>{
           {state.map((c) => {
             return (
               <div onClick={() => setCurrentChat(c)}>
-                <Conversation conversations={c} setConversation ={setConversation} userId = {userId} />
+                <Conversation
+                  conversations={c}
+                  setConversation={setConversation}
+                  userId={userId}
+                />
               </div>
             );
           })}
@@ -91,17 +112,23 @@ useEffect(()=>{
         {currentChat ? (
           <>
             <div className={styles.dialogues}>
-              {message.map((m)=>{
-                return(
-                  <Message message = {m}/>
-
-                )
+              {tryMessages?.map((m) => {
+                return <Message message={m} />;
               })}
-              
+
               <div className={styles.inpWrapper}>
                 <div className={styles.inpBtn}>
-                  <input placeholder="Введите сообщение.." type="text" value={newMessages} onChange={(e)=> setNewMessages(e.target.value)}/>
-                  <button onClick={()=> handleSubmit(currentChat._id, newMessages)}>Отправить</button>
+                  <input
+                    placeholder="Введите сообщение.."
+                    type="text"
+                    value={newMessages}
+                    onChange={(e) => setNewMessages(e.target.value)}
+                  />
+                  <button
+                    onClick={() => handleSubmit(currentChat._id, newMessages)}
+                  >
+                    Отправить
+                  </button>
                 </div>
               </div>
             </div>
